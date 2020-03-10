6b73e80629bcce6c1fd07fa8343993ce8bed4145
thienqui-communistspy <professionalbusiness505@gmail.com>
2020-03-10 12:12:55 -0400

    Made changes to contributions

--- app/routes/session.js
+++ app/routes/session.js
@@ -59,9 +59,10 @@ function SessionHandler(db) {
             var errorMessage = "Invalid username and/or password";
             var invalidUserNameErrorMessage = "Invalid username";
             var invalidPasswordErrorMessage = "Invalid password";
+            var ESAPI = require('node-esapi');
             if (err) {
                 if (err.noSuchUser) {
-                    console.log('Error: attempt to login with invalid user: ', userName);
+                  console.log('Error: attempt to login with invalid user: %s', userName.replace(/(\r\n|\r|\n)/g, '_'));
 
                     // Fix for A1 - 3 Log Injection - encode/sanitize input for CRLF Injection
                     // that could result in log forging:
@@ -78,7 +79,7 @@ function SessionHandler(db) {
                     return res.render("login", {
                         userName: userName,
                         password: "",
-                        loginError: invalidUserNameErrorMessage
+                        loginError: errorMessage
                         //Fix for A2-2 Broken Auth - Uses identical error for both username, password error
                         // loginError: errorMessage
                     });
@@ -86,7 +87,7 @@ function SessionHandler(db) {
                     return res.render("login", {
                         userName: userName,
                         password: "",
-                        loginError: invalidPasswordErrorMessage
+                        loginError: errorMessage
                         //Fix for A2-2 Broken Auth - Uses identical error for both username, password error
                         // loginError: errorMessage
 
